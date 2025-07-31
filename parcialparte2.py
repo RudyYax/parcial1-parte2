ced9bfd99a7da36bea9055f1090fe0162c57a2e7
Rudy <rudyax2004@gmail.com>
2025-07-30 18:57:36 -0600

    quinto commit

--- parcialparte2.py
+++ parcialparte2.py
@@ -21,28 +21,35 @@ def Calcular(digitos):
     if digitos < 10:
         return digitos
     else:
-        return  Calcular(digitos // 10)
-
-
+        e = digitos // 10
+        return 1 + Calcular(e)
 
 
 print("Parcial parte 2")
 opcion = 0
 
 while opcion != 6:
-    print("Bienvenidos a esta parte del parcial")
+    print("\n Bienvenidos a esta parte del parcial")
     print("1.- Calcular el MCD de dos números")
     print("2.- Crear una cadena repetitiva N veces")
     print("3.- Contar cuántas veces aparece una letra en una cadena")
     print("4.- Convertir un numero decimal a binario")
     print("5.- Calcular cuantos dígitos tiene un numero")
-    opcion = int(input("Ingrese la opcion que desea"))
+    opcion = int(input("Ingrese la opcion que desea: "))
     match opcion:
         case 1:
             print("\n Calcular el MCD de dos numeros")
             numero1 = int(input("Ingrese el primer numero: "))
-            numero2 = int(input("Ingrese el segundo numero: "))
-            print("El resultado es: ", MCD(numero1, numero2))
+            if numero1 <= 0:
+                print("el numero no puede ser negativo")
+                break
+            else:
+                numero2 = int(input("Ingrese el segundo numero: "))
+                if numero2 <= 0:
+                    print("el numero no puede ser negativo")
+                    break
+                else:
+                    print("El MCD(Maximo Común Divisor es: ", MCD(numero1, numero2))
         case 2:
             print("\n crear una cadena repetitiva N veces")
             palabra1 = input("Ingrese la palabra: ")
